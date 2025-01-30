2be41489c844e17af9c60db812c2196e35ce3807
River <26424577+wusatosi@users.noreply.github.com>
2025-01-30 14:36:14 -0500

    update wording

--- cmake/msvc-toolchain.cmake
+++ cmake/msvc-toolchain.cmake
@@ -26,7 +26,7 @@ if(BEMAN_BUILDSYS_SANITIZER STREQUAL "MaxSan")
     set(SANITIZER_FLAGS "/fsanitize=address /Zi")
 endif()
 
-# /Zc:__cplusplus is needed for us to detect C++ version support correctly
+# /Zc:__cplusplus is needed for us to detect C++ version support portably
 # See: https://stackoverflow.com/questions/60052108/how-to-check-c-version-in-microsoft-visual-studio-2017
 set(BASE_FLAGS "/Zc:__cplusplus /EHsc /permissive-")
 
