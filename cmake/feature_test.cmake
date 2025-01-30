cc6621d8e226377c6b177b68f322c6150864e162
River <26424577+wusatosi@users.noreply.github.com>
2025-01-30 13:51:32 -0500

    add feature testing

---
+++ cmake/feature_test.cmake
@@ -0,0 +1,45 @@
+include(CheckCXXSourceCompiles)
+
+function(BEMAN_FEATURE_TEST FEATURE_TEST_EXP RESULT_VAR)
+    check_cxx_source_compiles(
+        "
+    #if (${FEATURE_TEST_EXP})
+    int main() {}
+    #else
+    #error
+    #endif
+    "
+        ${RESULT_VAR}
+    )
+endfunction()
+
+function(BEMAN_MIN_CXX_VERSION CXX_VERSION)
+    set(VERSION_MAP
+        11
+        201103L
+        14
+        201402L
+        17
+        201703L
+        20
+        202002L
+        23
+        202302L
+    )
+
+    list(FIND VERSION_MAP "${CXX_VERSION}" INDEX)
+    if(INDEX EQUAL -1)
+        message(FATAL_ERROR "Unkown required C++ version: ${CXX_VERSION}")
+    else()
+        math(EXPR INDEX "${INDEX} + 1")
+        list(GET VERSION_MAP ${INDEX} VERSION_DATE)
+    endif()
+
+    beman_feature_test("__cplusplus >= ${VERSION_DATE}" CXX_VERSION_MET)
+    if(NOT CXX_VERSION_MET)
+        message(
+            FATAL_ERROR
+            "This project requires a minimum C++ version of ${CXX_VERSION}"
+        )
+    endif()
+endfunction()
